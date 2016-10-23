b0ee809b6097403ce5972960a6c0ecc460b45296
justanirudh <anirudh.pathak1@gmail.com>
2016-10-22 23:35:30 -0400

    restructured LCS making map a part of the method hence making LPS work

--- Project1/src/com/anirudh/hackerrank/LongestCommonSubsequence.scala
+++ Project1/src/com/anirudh/hackerrank/LongestCommonSubsequence.scala
@@ -9,38 +9,27 @@ import scala.io.StdIn._
 object LongestCommonSubsequence extends App{
 //Dynamic Programming: Iteration/recursion + memoization
 
-  val mapString = new mutable.HashMap[(String, String), String]() //hashmap
-  val mapSeq = new mutable.HashMap[(Seq[String], Seq[String]), Seq[String]]() //hashmap
-
-  def getOrCalculate(key:(String, String)):String = {
-    if(mapString.contains(key))
-      mapString(key)
-    else{
-      val lcs = findLCSString(key._1, key._2)
-      mapString.put(key, lcs)
-      lcs
-    }
-  }
-
-  def getOrCalculateSeq(key:(Seq[String], Seq[String])):Seq[String] = {
-    if(mapSeq.contains(key))
-      mapSeq(key)
-    else{
-      val lcs = findLCSSeq(key._1, key._2)
-      mapSeq.put(key, lcs)
-      lcs
-    }
-  }
-
   def findLCSString(s1:String, s2:String):String = {
+    val mapString = new mutable.HashMap[(String, String), String]() //hashmap
+
+    def getOrCalculateLCS(key:(String, String)):String = {
+      if(mapString.contains(key))
+        mapString(key)
+      else{
+        val lcs = findLCSString(key._1, key._2)
+        mapString.put(key, lcs)
+        lcs
+      }
+    }
+
     if(s1.isEmpty || s2.isEmpty) //00 or 10 or 01
       ""
     else{
       if(s1.last == s2.last)
-        getOrCalculate((s1.init, s2.init)) + s1.last
+        getOrCalculateLCS((s1.init, s2.init)) + s1.last
       else{
-        val oneOffs1 = getOrCalculate((s1.init, s2))
-        val oneOffs2 = getOrCalculate((s1, s2.init))
+        val oneOffs1 = getOrCalculateLCS((s1.init, s2))
+        val oneOffs2 = getOrCalculateLCS((s1, s2.init))
         if(oneOffs1.length > oneOffs2.length)
           oneOffs1
         else
@@ -50,6 +39,18 @@ object LongestCommonSubsequence extends App{
   }
 
   def findLCSSeq(s1:Seq[String], s2:Seq[String]):Seq[String] = {
+    val mapSeq = new mutable.HashMap[(Seq[String], Seq[String]), Seq[String]]() //hashmap
+
+    def getOrCalculateSeq(key:(Seq[String], Seq[String])):Seq[String] = {
+      if(mapSeq.contains(key))
+        mapSeq(key)
+      else{
+        val lcs = findLCSSeq(key._1, key._2)
+        mapSeq.put(key, lcs)
+        lcs
+      }
+    }
+
     if(s1.isEmpty || s2.isEmpty) //00 or 10 or 01
       Seq()
     else{
@@ -66,13 +67,13 @@ object LongestCommonSubsequence extends App{
     }
   }
 
-//  val s1 = "ABCBDAB"
-//  val s2 = "BDCABA"
+  val s1 = "ABCBDABA"
+  val s2 = "BDCABA"
 
-  val lengths = readLine()
-  val s1 = readLine().split(" ")
-  val s2 = readLine().split(" ")
-  val lcs = findLCSSeq(s1, s2)
+//  val lengths = readLine()
+//  val s1 = readLine().split(" ")
+//  val s2 = readLine().split(" ")
+  val lcs = findLCSString(s1, s2)
 
   println(lcs.mkString(" "))
 
