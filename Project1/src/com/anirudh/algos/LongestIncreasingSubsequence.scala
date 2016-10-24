0065ca7dad18ed91910ff83cfda365b33fc77bac
justanirudh <anirudh.pathak1@gmail.com>
2016-10-23 23:01:59 -0400

    Fixed LCS: base cases for each entry

--- Project1/src/com/anirudh/algos/LongestIncreasingSubsequence.scala
+++ Project1/src/com/anirudh/algos/LongestIncreasingSubsequence.scala
@@ -6,23 +6,34 @@ package com.anirudh.algos
 object LongestIncreasingSubsequence extends App{
 
   def findLIS(nums:Seq[Int]):(Seq[Int], Int) = {
-    var allSeqs = Seq[(Seq[Int], Int)](( Seq(nums.head), 1))
+    //bottom-up approach
+    def findLISAux(nums:Seq[Int]):(Seq[Int], Int) = {
+      //initializing 1st seq
+      var allSeqs = Seq[(Seq[Int], Int)](( Seq(nums.head), 1))
 
-    for(i <- 1 until nums.size ){
-      var maxSeq = Seq[Int]()
-      var maxSize = 0
-      for(j <- 0 until i){
-        if(nums(j) < nums(i) && allSeqs(j)._2 + 1 > maxSize){
-          maxSeq = allSeqs(j)._1 :+ nums(i)
-          maxSize = allSeqs(j)._2 + 1
+      for(i <- 1 until nums.size ){ //starting from 2nd element of nums
+        var maxSeq = Seq[Int](nums(i))
+        var maxSize = 1
+        for(j <- 0 until i){
+          //comparing whether the num is < the current num && the length of the array increases
+          if(nums(j) < nums(i) && allSeqs(j)._2 + 1 > maxSize){
+            maxSeq = nums(i) +: allSeqs(j)._1 //so that prepending is O(1)
+            maxSize = allSeqs(j)._2 + 1
+          }
         }
+        allSeqs = allSeqs :+ (maxSeq, maxSize)
       }
-      allSeqs = allSeqs :+ (maxSeq, maxSize)
+      allSeqs.maxBy(_._2)
     }
-    allSeqs.maxBy(_._2)
+
+    val (revSeq, size) = findLISAux(nums)
+    (revSeq.reverse, size)
   }
 
   val lis  = findLIS(Seq(15, 27, 14, 38, 26, 55, 46, 65, 85))
+
+  println(lis._2)
+  println("-----------")
 
   lis._1 foreach println
 
