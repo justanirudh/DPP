30b86dd180098786601e6be60f7cf2b5f472419b
JustAnirudh <paanir@Anirudhs-MacBook-Pro.local>
2016-12-17 03:20:19 -0500

    create Largest number from given numbers

--- CodingPractice/src/com/anirudh/general_algos/LargestNumberS.scala
+++ CodingPractice/src/com/anirudh/general_algos/LargestNumberS.scala
@@ -3,6 +3,12 @@ package com.anirudh.general_algos
 /**
   * Created by paanir on 12/17/16.
   */
-object LargestNumberS {
+//Given an array of numbers, find the largest number you can make from it
+//Eg. [50, 213, 9] -> 950213
+object LargestNumberS extends App{
 
+  val arr = Seq(50, 213, 9)
+
+  val ans = arr.map(num => (num.toString.head.toInt, num)).sortBy(_._1).reverse.map(tuple => tuple._2).mkString("")
+  println(ans)
 }
