8fd41ba3235c7683bd6fe120a6af6370292a4cab
JustAnirudh <anirudh.pathak1@gmail.com>
2016-12-22 01:08:52 -0800

    make things right

--- CodingPractice/src/com/anirudh/general_algos/LargestNumber.java
+++ CodingPractice/src/com/anirudh/general_algos/LargestNumber.java
@@ -14,10 +14,9 @@ For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
 Note: The result may be very large, so you need to return a string instead of an integer.
  */
 
-//TODO: some tests failing
 public class LargestNumber {
     public static ArrayList<Integer> largestNumber(ArrayList<Integer> nums) {
-        TreeMap<Character, ArrayList<Integer>> sm = new TreeMap(); //sorted map
+        TreeMap<Character, ArrayList<Integer>> sm = new TreeMap<>(); //sorted map
         for (Integer i : nums) {
             char firstDigit = i.toString().charAt(0);
             if (!sm.containsKey(firstDigit))
@@ -26,28 +25,29 @@ public class LargestNumber {
         }
 
         ArrayList<Character> sortedKeys = new ArrayList<Character>();
-
         for (Character k : sm.keySet())
             sortedKeys.add(k);
 
-        ArrayList<Integer> al = new ArrayList<>();
+        ArrayList<Integer> al = new ArrayList<>(); //reverse sorted list of integers by their (recursive) first digits
 
         for (int i = sortedKeys.size() - 1; i >= 0; --i) {
             Character key = sortedKeys.get(i);
             ArrayList<Integer> values = sm.get(key);
-            if (values.size() == 1)
+            if (values.size() == 1) //if value array has only 1 number, just append it
                 al.add(values.get(0));
             else {
                 int singleDigitCount = 0;
                 ArrayList<Integer> newValues = new ArrayList<>();
-                for (Integer v : values) {
-                    if (v == v % 10) {
+                for (Integer v : values) { //if 3 -> 3, 32 ,34; remove 3 and add 2 and 4 to next recursive call
+                    Integer firstpart = Character.getNumericValue(v.toString().charAt(0));
+                    if (v.intValue() == firstpart.intValue())
                         ++singleDigitCount;
-                    } else {
-                        newValues.add(v % 10);
+                    else {
+                        Integer secondPart = Integer.parseInt(v.toString().substring(1));
+                        newValues.add(secondPart);
                     }
                 }
-                ArrayList<Integer> temp = largestNumber(newValues); // /10
+                ArrayList<Integer> temp = largestNumber(newValues);
                 ArrayList<Integer> newTemp = new ArrayList<>();
                 if (singleDigitCount == 0) {
                     for (Integer t : temp) {
@@ -55,25 +55,46 @@ public class LargestNumber {
                         newTemp.add(newt);
                     }
                 } else {
-                    for (Integer t : temp) {
-                        Integer keyInt = Character.getNumericValue(key);
-                        if (t <= keyInt) {
+                    Integer keyInt = Character.getNumericValue(key);
+                    if (temp.size() == 0) {
+                        while (singleDigitCount != 0) {
+                            newTemp.add(keyInt);
+                            singleDigitCount--;
+                        }
+                    } else {
+                        for (Integer t : temp) {
+                            if (t <= keyInt) {
+                                while (singleDigitCount != 0) {
+                                    newTemp.add(keyInt);
+                                    singleDigitCount--;
+                                }
+                            }
+                            Integer newt = Integer.parseInt(key.toString() + t.toString());
+                            newTemp.add(newt);
+                        }
+                        if (singleDigitCount > 0) { //key is the lowest number, then add all repeated single digits at the end
                             while (singleDigitCount != 0) {
                                 newTemp.add(keyInt);
                                 singleDigitCount--;
                             }
                         }
-                        Integer newt = Integer.parseInt(key.toString() + t.toString());
-                        newTemp.add(newt);
                     }
-                    al.addAll(newTemp);
                 }
+                al.addAll(newTemp); // add to final array
             }
         }
         return al;
     }
 
     public static String auxLN(int[] nums) {
+        boolean allZeroes = true;
+        for (int num : nums) {
+            if (num != 0) {
+                allZeroes = false;
+            }
+        }
+        if (allZeroes)
+            return "0";
         ArrayList<Integer> intList = new ArrayList<Integer>();
         for (int index = 0; index < nums.length; index++) {
             intList.add(nums[index]);
@@ -87,7 +108,7 @@ public class LargestNumber {
     }
 
     public static void main(String[] args) {
-        int[] arr = {128, 12/*, 50, 99, 3*/};
+        int[] arr = {16, 128, 1};
         System.out.println(auxLN(arr));
     }
 
