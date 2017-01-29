ff7eff4b83d6ba6e37bc66e712d931db11f8c9fa
JustAnirudh <anirudh.pathak1@gmail.com>
2017-01-28 23:01:29 -0500

    2 sigma hackerank Question 2

---
+++ CodingPractice/src/competitions/TwoSigma_LongestChain.java
@@ -0,0 +1,65 @@
+package competitions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
+/**
+ * Created by paanir on 1/28/17.
+ */
+public class TwoSigma_LongestChain {
+
+    static int longestChain(String[] words) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        class comp implements Comparator<String> {
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(), o2.length());
+            }
+        }
+
+        Arrays.sort(words, new comp());
+
+        for (int i = 0; i < words.length; ++i) { /*****\n*****/
+            String curr = words[i];
+            if (curr.equals("")) {
+                map.put(curr, 1);
+            } else {
+                for (int j = 0; j < curr.length(); ++j) {
+                    StringBuilder sb = new StringBuilder(curr);
+                    String oneLessChar = sb.deleteCharAt(j).toString();
+                    if (map.containsKey(oneLessChar)) {
+                        if (!map.containsKey(curr)) //map doesnt have curr
+                            map.put(curr, map.get(oneLessChar) + 1);
+                        else { //map contains curr
+                            if (map.get(oneLessChar) + 1 > map.get(curr))
+                                map.put(curr, map.get(oneLessChar) + 1);
+                        }
+                    }
+                }
+                if (!map.containsKey(curr)) {
+                    map.put(curr, 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (Integer i : map.values()) {
+            if (i > max)
+                max = i;
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+
+        String[] arr = new String[]{"bda", "bca", "a", "b", "ba", "bdca"};
+
+        System.out.println(longestChain(arr));
+
+        ;
+
+    }
+}
