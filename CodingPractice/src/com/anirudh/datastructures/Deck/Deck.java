5df969ab91088928d8cde76ba9ce392faaa3f7c5
JustAnirudh <anirudh.pathak1@gmail.com>
2017-01-30 23:46:15 -0500

    OOD Card deck

---
+++ CodingPractice/src/com/anirudh/datastructures/Deck/Deck.java
@@ -0,0 +1,27 @@
+package com.anirudh.datastructures.Deck;
+
+/**
+ * Created by paanir on 1/30/17.
+ */
+public class Deck {
+
+    int deckSize = 10;
+
+    Card[] cards;
+
+    private static Deck d;
+
+    private Deck(Card[] cards) {
+        if (cards.length < deckSize)
+            throw new RuntimeException("min length 10 reqd");
+        this.cards = cards;
+    }
+
+    public static Deck getDeck(Card[] cards) {
+        if (d == null)
+            d = new Deck(cards);
+        else
+            System.out.println("Sorry, We have only 1 deck. Cannot use supplied cards");
+        return d;
+    }
+}
