4064696ea77259441a11fa5353fbdfac66243594
JustAnirudh <anirudh.pathak1@gmail.com>
2017-02-05 23:56:00 -0500

    LeetCode:239

---
+++ CodingPractice/src/com/anirudh/general_algos/SlidingWindowMaximum.java
@@ -0,0 +1,54 @@
+package com.anirudh.general_algos;
+
+/**
+ * Created by paanir on 2/5/17.
+ */
+/*
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+For example,
+Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Therefore, return the max sliding window as [3,3,5,5,6,7].
+
+Note:
+You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
+
+ */
+
+public class SlidingWindowMaximum {
+
+    //O((n-k) * k )
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0)
+            return new int[0];
+        int[] maxes = new int[nums.length - k + 1];
+        for (int i = 0; i <= nums.length - k; ++i) {
+            System.out.println("here");
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; ++j) {
+                if (nums[j] > max)
+                    max = nums[j];
+            }
+            maxes[i] = max;
+        }
+        return maxes;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = maxSlidingWindow(new int[]{1}, 1);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
