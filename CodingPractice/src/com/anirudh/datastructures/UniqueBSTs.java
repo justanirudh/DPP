abd7f539d40eebbd0e3bcd41c4a852dff29ce0fd
JustAnirudh <anirudh.pathak1@gmail.com>
2017-02-06 23:48:22 -0500

    LeetCode: 96

---
+++ CodingPractice/src/com/anirudh/datastructures/UniqueBSTs.java
@@ -0,0 +1,28 @@
+package com.anirudh.datastructures;
+
+/**
+ * Created by paanir on 2/6/17.
+ */
+public class UniqueBSTs {
+
+    public static long nChooseK(int n, int k) {
+        if (k > n - k) //nCk = nC(n-k)
+            k = n - k;
+
+        //(n * (n-1) * .....(n - (k - 1))) / 1 * 2 * ... * k
+        long num = 1;
+        for (int i = 0; i < k; ++i) {
+            num *= (n - i); //n to n - (k - 1)
+            num /= (i + 1); //1 to k
+        }
+        return num;
+    }
+
+    public static long numTrees(int n) { //catalan nums = 2nCn / (n+1)
+        return nChooseK(2 * n, n) / (n + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numTrees(19));
+    }
+}
