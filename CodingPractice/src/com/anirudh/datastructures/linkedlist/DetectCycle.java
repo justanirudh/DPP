2b848acace7517fc18eaef50470f79b6b757eeb8
JustAnirudh <anirudh.pathak1@gmail.com>
2017-02-15 23:49:41 -0500

    LeetCode #142

---
+++ CodingPractice/src/com/anirudh/datastructures/linkedlist/DetectCycle.java
@@ -0,0 +1,58 @@
+package com.anirudh.datastructures.linkedlist;
+
+/**
+ * Created by paanir on 2/15/17.
+ */
+/*
+142. Linked List Cycle II
+Description  Submission  Solutions  Add to List
+Total Accepted: 102915
+Total Submissions: 331607
+Difficulty: Medium
+Contributors: Admin
+
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+ */
+public class DetectCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode slow = head.next;
+        ListNode fast = null;
+        if (slow == null)
+            return null;
+        else
+            fast = slow.next;
+        while (slow != null && fast != null && slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast == null)
+                break;
+            else
+                fast = fast.next;
+        }
+        if (slow == null || fast == null)
+            return null;
+        //slow == fast
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
