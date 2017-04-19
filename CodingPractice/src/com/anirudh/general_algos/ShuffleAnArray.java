cab0613f4735058da7554588d932fda10fa503e7
JustAnirudh <anirudh.pathak1@gmail.com>
2017-04-18 23:58:47 -0400

    LeetCode #384 - awesome approach

--- CodingPractice/src/com/anirudh/general_algos/ShuffleAnArray.java
+++ CodingPractice/src/com/anirudh/general_algos/ShuffleAnArray.java
@@ -1,70 +1,62 @@
 package com.anirudh.general_algos;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by paanir on 4/17/17.
  */
+/*
+384. Shuffle an Array
+Shuffle a set of numbers without duplicates.
 
-//#384.Naive. Under-construction
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+ */
+import java.util.Random;
+
 public class ShuffleAnArray {
-    List<List<Integer>> shuffles;
+
     int[] nums;
+    Random r;
 
-    private List<Integer> addElement(List<Integer> arr, int num, int index) {
-        List<Integer> newList = new ArrayList<Integer>(arr);
-        newList.add(index, num);
-        return newList;
-    }
-
-    private List<List<Integer>> permute(int[] nums) {
-
-        List<List<Integer>> permutations = new ArrayList<>();
-        if (nums.length == 0)
-            return permutations;
-
-        ArrayList<Integer> first = new ArrayList<>();
-        first.add(nums[0]);
-        permutations.add(first);
-
-        for (int n = 1; n < nums.length; ++n) {
-            int num = nums[n];
-            List<List<Integer>> permsTemp = new ArrayList<>();
-
-            for (List<Integer> arr : permutations) {
-                for (int i = 0; i <= arr.size(); i++) {
-                    List<Integer> newArr = addElement(arr, num, i);
-                    permsTemp.add(newArr);
-                }
-            }
-            permutations = permsTemp;
-        }
-        return permutations;
-    }
-
-    private int[] toIntArray(List<Integer> list){
-        int[] ret = new int[list.size()];
-        for(int i = 0;i < ret.length;i++)
-            ret[i] = list.get(i);
-        return ret;
+    private void swap(int[] arr, int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
     }
 
     public ShuffleAnArray(int[] nums) {
-        this.nums = nums;
-        shuffles = permute(nums);
+        this.nums = nums; //dont touch original array
+        r = new Random();
     }
 
-    /** Resets the array to its original configuration and return it. */
+    /**
+     * Resets the array to its original configuration and return it.
+     */
     public int[] reset() {
         return this.nums;
     }
 
-    /** Returns a random shuffling of the array. */
-    public int[] shuffle() {
-        int rand_indx = (int )(Math.random() * 50); //0 to 49
-        List<Integer> shuffled = shuffles.get(rand_indx);
-
-        return toIntArray(shuffled);
+    /**
+     * Returns a random shuffling of the array.
+     */
+    public int[] shuffle() { //can also directly use Colllections.shuffle. This one's smarter
+        int[] newArr = this.nums.clone();
+        for (int i2 = 1; i2 < newArr.length; ++i2) {// i2 is RHS border, i1 can be b/w 0 and i2-inclusive
+            int i1 = r.nextInt(i2 + 1); //get random number between [0, i2]
+            swap(newArr, i1, i2);
+        }
+        return newArr;
     }
+
 }
