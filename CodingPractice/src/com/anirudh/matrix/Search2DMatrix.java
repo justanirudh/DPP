5ccd32ea2aa622ac74b1cb7085f3de7ad32d3c9a
JustAnirudh <anirudh.pathak1@gmail.com>
2017-04-24 23:58:07 -0400

    Refactoring

--- CodingPractice/src/com/anirudh/matrix/Search2DMatrix.java
+++ CodingPractice/src/com/anirudh/matrix/Search2DMatrix.java
@@ -1,10 +1,27 @@
-package com.anirudh.general_algos;
+package com.anirudh.matrix;
 
 /**
  * Created by paanir on 4/22/17.
  */
 
-//LeetCode#74. Under-construction
+/*
+74. Search a 2D Matrix
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+Given target = 3, return true.
+ */
+//O(log(mn))
 public class Search2DMatrix {
     int[][] matrix;
     int target;
@@ -23,35 +40,34 @@ public class Search2DMatrix {
             return binSearch(startCol, endCol - 1);
     }
 
-    public boolean matSearch(int row, int col) {
+    public boolean matSearch(int start, int end) {
+        int row = (start + end) / 2;
+        int col = matrix[0].length / 2;
         if (row < 0 || row == matrix.length || col < 0 || col == matrix[0].length)
             return false;
-        if(track[row] == 0){
+        if (track[row] == 0) {
             track[row] = 1;
-        }
-        else
+        } else
             return false; //already checked this row.
         int curr = matrix[row][col];
         if (curr == target)
             return true;
-        if ( target < curr) {
+        if (target < curr) {
             if (target >= matrix[row][0]) {
                 currRow = matrix[row];
-                System.out.println(row);
                 return binSearch(0, col - 1);
-            } else{
-                if(row == 0)
+            } else {
+                if (row == 0)
                     return false;
-                return matSearch((row - 1) / 2, col / 2);
+                return matSearch(start, row - 1);
             }
 
         } else { // target > curr
             if (target <= matrix[row][matrix[0].length - 1]) {
                 currRow = matrix[row];
-                System.out.println(row);
                 return binSearch(col + 1, matrix[0].length - 1);
             } else {
-                return matSearch((row + 1 + matrix.length) / 2, col / 2);
+                return matSearch(row + 1, end);
             }
         }
     }
@@ -62,17 +78,16 @@ public class Search2DMatrix {
         this.matrix = matrix;
         this.target = target;
         this.track = new int[matrix.length];
-        return matSearch(matrix.length / 2, matrix[0].length / 2);
+        return matSearch(0, matrix.length);
     }
 
     public static void main(String[] args) {
         Search2DMatrix s = new Search2DMatrix();
-        int[][] mat = {
-                {1, 3, 5, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 50}
-        };
-        int target = 3;
+        int[][] mat = {{-10, -8}, {-6, -5}, {-2, -2}, {-1, 0}, {3, 4}, {7, 7}, {8, 9}, {10, 10}, {11, 11}, {12, 14},
+                {15, 16}, {17, 19}, {20, 21}, {22, 22}, {25, 27}, {28, 30}, {32, 32}, {35, 36}};
+        int target = 16;
         System.out.println(s.searchMatrix(mat, target));
     }
+
+    //SOLUTION 2: Treat it as a sorted array, not a matrix!! O(log(m+n))
 }
