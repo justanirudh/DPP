a3ba7917f83953e9cd9c47eff5f1fbf3596e29f3
JustAnirudh <anirudh.pathak1@gmail.com>
2017-06-07 23:26:04 -0700

    LeetCode #78

---
+++ CodingPractice/src/com/anirudh/math/Subsets.java
@@ -0,0 +1,54 @@
+package com.anirudh.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by paanir on 6/7/17.
+ */
+/*
+78. Subsets
+
+Given a set of distinct integers, nums, return all possible subsets.
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+ */
+/*
+Idea is to go through all numbers from 0 to 2^nums.length
+for each of those numbers, its bit representation would give us the subset
+if the bit representation is 1 at a place, include the corresponding number in nums array, else dont
+ */
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        int bitLength = nums.length;
+        int max = (int) Math.pow(2, bitLength);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int arrg = 0; arrg < max; ++arrg) { //arrg = arrangement;
+            List<Integer> subset = new ArrayList<>();
+            //populate list
+
+            for (int bitIndex = 0; bitIndex < bitLength; ++bitIndex) {
+                int include = ((arrg >> bitIndex) & 1); //bit at given bit index
+                if (include == 1)
+                    subset.add(nums[bitIndex]);
+            }
+            res.add(subset);
+        }
+        return res;
+    }
+}
