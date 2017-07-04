b6c9093ab6bc48281628dc3cfd6341c293945b74
JustAnirudh <anirudh.pathak1@gmail.com>
2017-07-03 23:39:45 -0700

    LC #55 under-construction

---
+++ CodingPractice/src/com/anirudh/dynamic_programming/JumpGame.java
@@ -0,0 +1,14 @@
+package com.anirudh.dynamic_programming;
+
+import java.util.HashMap;
+
+/**
+ * Created by paanir on 7/3/17.
+ */
+//LC:#55 under-construction
+public class JumpGame {
+    HashMap<Integer, Integer> indexToWays = new HashMap<>();
+    public boolean canJump(int[] nums) {
+
+    }
+}
