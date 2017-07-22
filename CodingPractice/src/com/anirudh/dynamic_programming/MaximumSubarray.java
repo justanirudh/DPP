cc1e9312980f6f0806dd7805ac6de374124a62c4
JustAnirudh <anirudh.pathak1@gmail.com>
2017-07-21 23:42:31 -0700

    LeetCode #53 DP

--- CodingPractice/src/com/anirudh/dynamic_programming/MaximumSubarray.java
+++ CodingPractice/src/com/anirudh/dynamic_programming/MaximumSubarray.java
@@ -12,25 +12,41 @@ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
  */
 public class MaximumSubarray {
 
+    //DP, O(n) solution
+    public int maxSubArray(int[] nums) {
+
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+
+        for (int i = 1; i < nums.length; ++i) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+
+    //------------------------------------------
     public int maxSubArrayNaive(int[] nums) {
         //brute-force, O(n^2) solution
         //TLE
 
         int[][] mat = new int[nums.length][nums.length];
-        for(int i = 0; i < mat.length; ++i)
+        for (int i = 0; i < mat.length; ++i)
             mat[i][0] = nums[i];
 
         int maxSum = mat[0][0];
-        for(int i = 0; i < mat.length; ++i){
-            for(int j = 1; j < mat[0].length; ++j){
-                if(i >= j)
-                    mat[i][j] = mat[i][j-1];
+        for (int i = 0; i < mat.length; ++i) {
+            for (int j = 1; j < mat[0].length; ++j) {
+                if (i >= j)
+                    mat[i][j] = mat[i][j - 1];
                 else
-                    mat[i][j] = mat[i][j-1] + nums[j];
-                if(mat[i][j] > maxSum)
+                    mat[i][j] = mat[i][j - 1] + nums[j];
+                if (mat[i][j] > maxSum)
                     maxSum = mat[i][j];
             }
         }
         return maxSum;
     }
+
 }
