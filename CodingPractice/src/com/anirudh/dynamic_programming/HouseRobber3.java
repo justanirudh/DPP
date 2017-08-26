5bd94f7082ab616f8f840832020f86b820ff896f
JustAnirudh <anirudh.pathak1@gmail.com>
2017-08-25 23:31:25 -0400

    LeetCode #337

--- CodingPractice/src/com/anirudh/dynamic_programming/HouseRobber3.java
+++ CodingPractice/src/com/anirudh/dynamic_programming/HouseRobber3.java
@@ -3,57 +3,59 @@ package com.anirudh.dynamic_programming;
 /**
  * Created by paanir on 8/21/17.
  */
+/*
+337. House Robber III
 
+The thief has found himself a new place for his thievery again. There is only one entrance to this area,
+called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart
+thief realized that "all houses in this place forms a binary tree". It will automatically contact the police
+if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.
+ */
 import com.anirudh.datastructures.trees.TreeNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 //LeetCode #337 Under-construction
 public class HouseRobber3 {
 
-    public class Node {
-        TreeNode tn;
-        int height;
-
-        Node(TreeNode tn, int height) {
-            this.tn = tn;
-            this.height = height;
-        }
+    public int rob(TreeNode root) {
+        return robSub(root, new HashMap<>());
     }
 
-    public int rob(TreeNode root) {
-        //get sum of all nodes in the same level, make an array of it
-        //now the probem is similar to HouseRobber-1
-        if (root == null)
-            return 0;
+    private int robSub(TreeNode root, Map<TreeNode, Integer> map) {
+        if (root == null) return 0;
+        if (map.containsKey(root)) return map.get(root);
 
-        List<Integer> levelSums = new ArrayList<>();
-        levelSums.add(0);
+        int val = 0;
 
-        Node node = new Node(root, 0);
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(node);
-
-        while (!queue.isEmpty()) {
-            Node head = queue.remove();
-            if (head.tn.left != null)
-                queue.add(new Node(head.tn.left, head.height + 1));
-            if (head.tn.right != null)
-                queue.add(new Node(head.tn.right, head.height + 1));
-
-            levelSums.set(levelSums.size() - 1, levelSums.get(levelSums.size() - 1) + head.tn.val); //add head to last number
-
-            if (!queue.isEmpty() && queue.element().height == head.height + 1)
-                levelSums.add(0); //set stage for next level
+        if (root.left != null) {
+            val += robSub(root.left.left, map) + robSub(root.left.right, map);
         }
-        //now the problem is similar to HouseRobber-1
-        return new HouseRobber2().maxRob(levelSums.stream().mapToInt(i -> i).toArray(),
-                0,
-                levelSums.size() - 1);
 
+        if (root.right != null) {
+            val += robSub(root.right.left, map) + robSub(root.right.right, map);
+        }
+
+        val = Math.max(val + root.val, robSub(root.left, map) + robSub(root.right, map));
+        map.put(root, val);
+
+        return val;
     }
 }
