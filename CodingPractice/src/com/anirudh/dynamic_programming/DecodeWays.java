e24e78d7f7c6dc1cc9638cba45916a1ca3254cf4
JustAnirudh <anirudh.pathak1@gmail.com>
2017-09-05 00:22:46 -0400

    Fixed

--- CodingPractice/src/com/anirudh/dynamic_programming/DecodeWays.java
+++ CodingPractice/src/com/anirudh/dynamic_programming/DecodeWays.java
@@ -19,7 +19,6 @@ Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
 
 The number of ways decoding "12" is 2.
  */
-//(UC)
 public class DecodeWays {
     public int numDecodings(String s) {
         if (s == null || s.equals(""))
@@ -27,18 +26,20 @@ public class DecodeWays {
         char[] chars = s.toCharArray();
         int[] ways = new int[s.length()];
         //ways[i] = number of ways to decode string till i (included)
-        //ways[s.length()] will be answer
-        // ways[0] = 1; //empty string
+        //ways[s.length() - 1] will be answer
         ways[0] = chars[0] != '0' ? 1 : 0; //if it is non-zero, it gets decoded
         for (int i = 1; i < s.length(); ++i) {
             //single digit
             if (chars[i] != '0') //chars i one behind ways i
-                ways[i] = ways[i - 1]; //if non-zero, then at least nmber of ways till i-1 valid as chars[i] also valid
+                ways[i] = ways[i - 1]; //if non-zero, then at least number of ways till i-1 valid as chars[i] also valid
             //double digit
             int doubleDigit = Integer.parseInt(new StringBuilder().append(chars[i - 1]).append(chars[i]).toString());
-            System.out.println(doubleDigit);
-            if (doubleDigit >= 10 && doubleDigit <= 26)
-                ways[i] += ways[i - 1]; //all ways till i-2 also valid as chars[i-1]chars[i] is also an integer
+            if (doubleDigit >= 10 && doubleDigit <= 26) {
+                if (i - 2 >= 0)
+                    ways[i] += ways[i - 2]; //all ways till i-2 also valid as chars[i-1]chars[i] is also an integer
+                else //if string is shorter
+                    ways[i] += 1;
+            }
         }
         return ways[s.length() - 1];
     }
