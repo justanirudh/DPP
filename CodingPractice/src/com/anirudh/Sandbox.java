bf24aa4f3c65b21f6fdb53abd4aaef98a6f711f5
JustAnirudh <anirudh.pathak1@gmail.com>
2017-09-16 18:28:02 -0400

    Tic-Tac-Toe

--- CodingPractice/src/com/anirudh/Sandbox.java
+++ CodingPractice/src/com/anirudh/Sandbox.java
@@ -1,7 +1,11 @@
 package com.anirudh;
 
+import com.sun.deploy.util.StringUtils;
+import com.sun.tools.javac.util.ArrayUtils;
+
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,6 +19,11 @@ public class Sandbox {
     public static void main(String[] args) {
         System.out.println(Math.pow(2,5));
         System.out.println(2 << 4);
+
+        String[] foo = {"2", "1"};
+        List<Integer> bar = Arrays.stream(foo).map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+//        int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7};
+//        String result = StringUtils.join(ArrayUtils.toObject(arr), " - ");
 
     }
 
