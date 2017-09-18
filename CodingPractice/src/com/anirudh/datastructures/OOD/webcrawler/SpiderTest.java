0c93e93a1083e3e95df848d195b0795b1a5a4b4e
JustAnirudh <anirudh.pathak1@gmail.com>
2017-09-17 23:56:16 -0400

    TripleByte prep

---
+++ CodingPractice/src/com/anirudh/datastructures/OOD/webcrawler/SpiderTest.java
@@ -0,0 +1,20 @@
+package com.anirudh.datastructures.OOD.webcrawler;
+
+/**
+ * Created by paanir on 9/17/17.
+ */
+public class SpiderTest
+{
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     *
+     * @param args
+     *            - not used
+     */
+    public static void main(String[] args)
+    {
+        Spider spider = new Spider();
+        spider.search("http://blog.teamtreehouse.com/passing-google-interview-without-computer-science-degree",
+                "parker");
+    }
+}
