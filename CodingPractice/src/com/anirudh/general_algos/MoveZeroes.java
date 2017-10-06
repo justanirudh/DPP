1501fc0a7d4241e0b0bf321a6faa939cc26c1ad6
JustAnirudh <anirudh.pathak1@gmail.com>
2017-10-06 00:59:57 -0400

    LeetCode #283

---
+++ CodingPractice/src/com/anirudh/general_algos/MoveZeroes.java
@@ -0,0 +1,37 @@
+package com.anirudh.general_algos;
+
+/**
+ * Created by paanir on 10/5/17.
+ */
+/*
+283. Move Zeroes
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+ */
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        while (true) {
+            //fast pointer helps find all non-zero elements and together with slow, accumulate it in the start
+            while (fast != nums.length && nums[fast] == 0)
+                fast++;
+            if (fast == nums.length)
+                break;
+            nums[slow] = nums[fast];
+            slow++;
+            fast++;
+        }
+        while (slow != nums.length) {
+            nums[slow] = 0;
+            slow++;
+        }
+        return;
+    }
+}
