2fb2fb74aacca08d940a126786073fc3e492332c
JustAnirudh <anirudh.pathak1@gmail.com>
2017-10-17 23:54:56 -0400

    Better solution

--- CodingPractice/src/com/anirudh/general_algos/SlidingWindowMaximum.java
+++ CodingPractice/src/com/anirudh/general_algos/SlidingWindowMaximum.java
@@ -4,7 +4,8 @@ package com.anirudh.general_algos;
  * Created by paanir on 2/5/17.
  */
 /*
-Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very
+right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
 For example,
 Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
@@ -23,11 +24,12 @@ Note:
 You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
 
  */
-
+//better algo: http://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/
+    //USe AVL/Red-black tree: O((n-k)logk)
 public class SlidingWindowMaximum {
 
     //O((n-k) * k )
-    public static int[] maxSlidingWindow(int[] nums, int k) {
+    public static int[] maxSlidingWindowBad(int[] nums, int k) {
         if (nums.length == 0)
             return new int[0];
         int[] maxes = new int[nums.length - k + 1];
@@ -43,9 +45,10 @@ public class SlidingWindowMaximum {
         return maxes;
     }
 
-    public static int[] maxSlidingWindowBetter(int[] nums, int k) {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
         if (nums.length == 0)
             return new int[0];
+
         int[] maxes = new int[nums.length - k + 1];
 
         int maxIndex = 0;
@@ -62,14 +65,12 @@ public class SlidingWindowMaximum {
             int newIndex = i + k - 1;
             int newNum = nums[newIndex];
             if (maxIndex >= i && maxIndex < newIndex) {
-                System.out.println("here");
                 if (newNum > maxValue) {
                     maxValue = newNum;
                     maxIndex = newIndex;
                 }
                 maxes[i] = maxValue;
             } else {
-                System.out.println("there");
                 int max = Integer.MIN_VALUE;
                 for (int j = i; j < i + k; ++j) {
                     if (nums[j] > max) {
@@ -85,7 +86,7 @@ public class SlidingWindowMaximum {
     }
 
     public static void main(String[] args) {
-        int[] arr = maxSlidingWindowBetter(new int[]{1, 3, 1, 2, 0 ,5}, 3);
+        int[] arr = maxSlidingWindow(new int[]{1, 3, 1, 2, 0, 5}, 3);
         for (int i : arr) {
             System.out.println(i);
         }
