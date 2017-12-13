648be14fc67a4908a8e010488a7c5a74a36f62f8
Anirudh <anirudh.pathak1@gmail.com>
2017-12-12 23:44:49 -0500

    LeetCode #617

---
+++ CodingPractice/src/com/anirudh/datastructures/trees/MergeTwoBinaryTrees.java
@@ -0,0 +1,46 @@
+package com.anirudh.datastructures.trees;
+
+/**
+ * Created by paanir on 12/12/17.
+ */
+/*
+617. Merge Two Binary Trees
+DescriptionHintsSubmissionsDiscussSolution
+Discuss Pick One
+Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees
+are overlapped while the others are not.
+
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up
+as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+
+Example 1:
+Input:
+	Tree 1                     Tree 2
+          1                         2
+         / \                       / \
+        3   2                     1   3
+       /                           \   \
+      5                             4   7
+Output:
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \
+	 5   4   7
+Note: The merging process must start from the root nodes of both trees.
+ */
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null)
+            return null;
+        else if (t1 == null || t2 == null)
+            return t1 == null ? t2 : t1;
+        else { //both non-null
+            TreeNode root = new TreeNode(t1.val + t2.val);
+            root.left = mergeTrees(t1.left, t2.left);
+            root.right = mergeTrees(t1.right, t2.right);
+            return root;
+        }
+    }
+}
