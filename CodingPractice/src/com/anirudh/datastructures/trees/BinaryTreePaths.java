6c02946951d6733264937557f36328c380ae4296
Anirudh <anirudh.pathak1@gmail.com>
2019-06-01 00:02:44 -0700

    Better implementation
    Signed-off-by: Anirudh <anirudh.pathak1@gmail.com>

--- CodingPractice/src/com/anirudh/datastructures/trees/BinaryTreePaths.java
+++ CodingPractice/src/com/anirudh/datastructures/trees/BinaryTreePaths.java
@@ -1,5 +1,7 @@
 package com.anirudh.datastructures.trees;
 
+import apple.laf.JRSUIUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +10,7 @@ import java.util.List;
  */
 /*
 257. Binary Tree Paths
-Given a binary tree, return all root-to-leaf paths.
+Given a binary tree, return all node-to-leaf paths.
 
 For example, given the following binary tree:
 
@@ -17,53 +19,93 @@ For example, given the following binary tree:
 2     3
  \
   5
-All root-to-leaf paths are:
+All node-to-leaf paths are:
 
 ["1->2->5", "1->3"]
  */
 public class BinaryTreePaths {
 
-    public static List<String> getPaths(String yet, TreeNode node) {
-        if (node == null)
-            return new ArrayList<>();
+    public List<String> getBinaryPaths(TreeNode node, String str, List<String> result) {
 
-        String curr = yet + "->" +Integer.toString(node.val);
-
-        List<String> lefts = getPaths(curr, node.left);
-
-        List<String> rights = getPaths(curr, node.right);
-
-        if(lefts.isEmpty() && rights.isEmpty()){
-            List<String> l = new ArrayList<>();
-            l.add(curr);
-            return l;
-        }
-        else{
-            lefts.addAll(rights);
-            return lefts;
+        if (node == null) { //can only be root as we will never traverse to a null node because of checks below
+            return result;
         }
 
+        String tillNow;
+        if (str == null) //root
+            tillNow = Integer.toString(node.val);
+        else
+            tillNow = str + "->" + node.val;
+
+        if (node.left == null && node.right == null) { //if both null, we have reached a leaf
+            result.add(tillNow);
+            return result;
+        } else if (node.left != null && node.right == null) { //if left is not null and right is null, only go to left
+            return getBinaryPaths(node.left, tillNow, result);
+        } else if (node.left == null) {  //if left is null and right is not null, only go to right
+            return getBinaryPaths(node.right, tillNow, result);
+        } else { //if both children non-null, go to both
+            List<String> leftResult = getBinaryPaths(node.left, tillNow, result); //get left result and pass it to right
+            return  getBinaryPaths(node.right, tillNow, leftResult);
+        }
     }
-
 
     public List<String> binaryTreePaths(TreeNode root) {
-        if (root == null)
-            return new ArrayList<>();
-        String curr = Integer.toString(root.val);
-
-        List<String> lefts = getPaths(curr, root.left);
-
-        List<String> rights = getPaths(curr, root.right);
-
-        if(lefts.isEmpty() && rights.isEmpty()){
-            List<String> l = new ArrayList<>();
-            l.add(curr);
-            return l;
-        }
-        else
-        {
-            lefts.addAll(rights);
-            return lefts;
-        }
+        List<String> result = new ArrayList<>();
+        return getBinaryPaths(root, null, result);
     }
 }
+
+
+
+
+/*
+
+class Solution {
+
+    List<String> paths;
+
+    public void addPath(TreeNode node, String tillNow){
+
+        if(node.left != null && node.right != null){
+            addPath(node.left, tillNow + node.val + "->");
+            addPath(node.right, tillNow + node.val + "->");
+
+        }
+        else if (node.left == null && node.right != null){
+            addPath(node.right, tillNow + node.val + "->");
+
+        }
+        else if (node.left != null && node.right == null){
+            addPath(node.left, tillNow + node.val + "->");
+        }
+        else{
+            paths.add(tillNow + node.val);
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        paths = new ArrayList<>();
+        if(root == null)
+            return paths;
+        String tillNow = "";
+        if(root.left != null && root.right != null){
+            addPath(root.left, tillNow + root.val + "->");
+            addPath(root.right, tillNow + root.val + "->");
+
+        }
+        else if (root.left == null && root.right != null){
+            addPath(root.right, tillNow + root.val + "->");
+
+        }
+        else if (root.left != null && root.right == null){
+            addPath(root.left, tillNow + root.val + "->");
+        }
+        else{
+            paths.add(tillNow + root.val);
+        }
+        return paths;
+
+    }
+}
+ */
