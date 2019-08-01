4615c85ea85e5cbe6938ac117969518a47cd6b82
Anirudh <anirudh.pathak1@gmail.com>
2019-07-31 19:34:58 -0700

    Use matrix traversal rather than creating adjacency list
    Signed-off-by: Anirudh <anirudh.pathak1@gmail.com>

--- CodingPractice/src/com/anirudh/datastructures/graphs/SurroundedRegions.java
+++ CodingPractice/src/com/anirudh/datastructures/graphs/SurroundedRegions.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 //BFS, can be done by DFS too
+
 /**
  * Created by paanir on 3/27/17.
  */
@@ -27,122 +28,67 @@ X X X X
 X O X X
 */
 public class SurroundedRegions {
+
     /*
-    In this problem, only the cells on the borders can not be surrounded.
-    So we replace O's with '#' if they are on borders, and then scan the board and replace all O's left (if any).
-     */
-    /*
-    http://www.programcreek.com/2014/04/leetcode-surrounded-regions-java/
-    Rather do BFS in the matrix, no need to make an adjacency list
-     */
-    private static int leftLim;
-    private static int rightLim;
-    private static int upLim;
-    private static int downLim;
+1. traverse through all graphs that start in the border //DFS to go through all graphs
+2. Populate the visited array
+3. iterate through the graph and convert all the non-visited Os (graphs of 0s that were not in the border) to X
+ */
+
+    private boolean[][] visited;
+    private char[][] board;
+    private int numRows;
+    private int numCols;
+    private int[] dx;
+    private int[] dy;
 
 
-    public static GraphNode createNode(char[][] board, Coord coord) {
-        GraphNode gn = new GraphNode(coord);
-        int x = coord.getX();
-        int y = coord.getY();
-
-        if (x - 1 >= upLim && board[x - 1][y] == 'O')
-            gn.addNeighbour(new Coord(x - 1, y));
-        if (x + 1 <= downLim && board[x + 1][y] == 'O')
-            gn.addNeighbour(new Coord(x + 1, y));
-        if (y - 1 >= leftLim && board[x][y - 1] == 'O')
-            gn.addNeighbour(new Coord(x, y - 1));
-        if (y + 1 <= rightLim && board[x][y + 1] == 'O')
-            gn.addNeighbour(new Coord(x, y + 1));
-
-        gn.setC('O');
-
-        return gn;
+    private boolean isBorderO(int i, int j) {
+        return board[i][j] == 'O' && (i == 0 || i == numRows - 1 || j == 0 || j == numCols - 1);
     }
 
-    public static HashMap<Coord, GraphNode> createGraph(char[][] board) {
-        HashMap<Coord, GraphNode> graph = new HashMap<>();
-        for (int x = 0; x < board.length; ++x) {
-            for (int y = 0; y < board[0].length; ++y) {
-                if (board[x][y] == 'O') { //if can be captured
-                    Coord coord = new Coord(x, y);
-                    GraphNode gn = createNode(board, coord);
-                    graph.put(coord, gn);
-                }
+    private boolean isValid(int i, int j) {
+        return i >= 0 && i < numRows && j >= 0 && j < numCols;
+    }
+
+    private void doDFS(int i, int j) {
+        visited[i][j] = true;
+        /*
+        Look at all 4 neighbours and keep converting to #s until we are donewith the DFS
+         */
+        for (int k = 0; k < 4; ++k) {
+            int x = i + dx[k];
+            int y = j + dy[k];
+            if (isValid(x, y) && board[x][y] == 'O' && !visited[x][y]) {
+                doDFS(x, y);
             }
         }
-        return graph;
     }
 
-    public static void doBFS(HashMap<Coord, GraphNode> graph, Coord coord) { //can also do dfs, doesnt matter
-        GraphNode node = graph.get(coord);
-//        node.setColor("gray");
-        node.setC('#');
-        Queue<Coord> queue = new LinkedList<>();
-        queue.add(coord);
-        while (!queue.isEmpty()) {
-            Coord c = queue.remove();
-            for (Coord child : graph.get(c).getNeighbours()) {
-                if (graph.get(child).getC() != '#') { //normal BFS: check if white
-                    graph.get(child).setC('#');
-                    queue.add(child);
-                }
-            }
-//            graph.get(c).setColor("black");
-        }
-    }
-
-    public static void solve(char[][] board) {
-        if (board.length == 0)
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0)
             return;
+        numRows = board.length;
+        numCols = board[0].length;
+        this.board = board;
+        dx = new int[]{0, 0, 1, -1};
+        dy = new int[]{1, -1, 0, 0};
 
-        leftLim = 0;
-        rightLim = board[0].length - 1;
-        upLim = 0;
-        downLim = board.length - 1;
+        visited = new boolean[numRows][numCols]; //all false
 
-        //create graph
-        HashMap<Coord, GraphNode> graph = createGraph(board);
-
-        //print graph
-//        for (Coord c : graph.keySet()) {
-//            System.out.print("(" + c.getX() + "," + c.getY() + "," + graph.get(c).getC() + ") -> ");
-//            for (Coord n : graph.get(c).getNeighbours()) {
-//                System.out.print("(" + n.getX() + "," + n.getY() + "), ");
-//            }
-//            System.out.println();
-//        }
-
-        //do bfs on each node on [the edge of the matrix && which is a O] and convert the values of all the traversed graph to '#'
-        for (Coord coord : graph.keySet()) {
-            int x = coord.getX();
-            int y = coord.getY();
-            if (x == upLim || x == downLim || y == leftLim || y == rightLim)
-                doBFS(graph, coord);
-        }
-
-        //print graph again
-//        for (Coord c : graph.keySet()) {
-//            System.out.print("(" + c.getX() + "," + c.getY() + "," + graph.get(c).getC() + ") -> ");
-//            for (Coord n : graph.get(c).getNeighbours()) {
-//                System.out.print("(" + n.getX() + "," + n.getY() + "), ");
-//            }
-//            System.out.println();
-//        }
-
-        //set new entries to table
-        for (Coord coord : graph.keySet()) {
-            char c = graph.get(coord).getC(); //either O or #
-            if (c != '#') //if not a boundary 0
-                board[coord.getX()][coord.getY()] = 'X'; //change it to captured
-        }
-
-        //print board
-        for (int x = 0; x < board.length; ++x) {
-            for (int y = 0; y < board[0].length; ++y) {
-                System.out.print(board[x][y] + ",");
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = 0; j < numCols; ++j) {
+                if (isBorderO(i, j)) {
+                    doDFS(i, j);
+                }
             }
-            System.out.println();
+        }
+
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = 0; j < numCols; ++j) {
+                if (board[i][j] == 'O' && !visited[i][j])
+                    board[i][j] = 'X';
+            }
         }
     }
 
@@ -153,6 +99,6 @@ public class SurroundedRegions {
                 {'X', 'O', 'O', 'X'},
                 {'X', 'O', 'X', 'X'}
         };
-        solve(g);
+        new SurroundedRegions().solve(g);
     }
 }
